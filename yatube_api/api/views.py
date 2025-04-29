195cdc037656bd260cfc0dae4f8a5f5ae9bfd81a
Niknet19 <nikitos.feo12@example.com>
2025-04-29 17:27:03 +0700

    Добавил аутентификацию по токену

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,1 +1,20 @@
-# TODO:  Напишите свой вариант
+from rest_framework import viewsets, permissions, exceptions, pagination
+from posts.models import Group, Post, Comment, Follow
+from .serializers import (
+    GroupSerializer,
+    PostSerializer,
+    CommentSerializer
+)
+
+
+class GroupViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
+
+class PostViewSet:
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+    pagination_class = pagination.LimitOffsetPagination
